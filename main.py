42f40b84d49f98f50dec5c9e4dc9df25b83845e2
Ricardo Ponce <alejandroponcetarazon@gmail.com>
2026-09-01 21:39:01 -0500

    Actualiza datos de la clase Persona

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 class Persona:
-    def __init__(self, nombre, edad):
+    def __init__(self, nombre, edad, profesion):
         self.nombre = nombre
         self.edad = edad
+        self.profesion = profesion
 
     def mostrar_datos(self):
         print(f"Nombre: {self.nombre}")
         print(f"Edad: {self.edad}")
+        print(f"Profesión: {self.profesion}")
 
 
-persona = Persona("Ricardo", 22)
+persona = Persona("Ricardo", 22, "Estudiante")
 persona.mostrar_datos()
